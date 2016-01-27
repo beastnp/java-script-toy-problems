8e64bf38d1c0b8f762bab326e5cfa4547f05612c
Nick Peterson <peterson.nick2@gmail.com>
2016-01-27 15:23:27 -0700

    looping through arrays

--- CodeCardioQuestions.js
+++ CodeCardioQuestions.js
@@ -250,7 +250,31 @@ function titleCase(str) {
 
 titleCase("I'm a little tea pot");
 
+19. //Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
 
+function largestOfFour(arr) {
+  var results = [];
+  for (var i = 0; i < arr.length; i++) {
+    var largestNumber = 0;
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+
+    results[i] = largestNumber;
+  }
+
+  return results;
+}
+
+//  second possible solution
+
+function largestOfFour(arr) {
+  return arr.map(Function.apply.bind(Math.max, null));
+}
+
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); should return [5, 27, 39, 1001]
 
 
 
