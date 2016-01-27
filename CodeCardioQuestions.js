bf0a3d46261fe2286efc379497e199acd31e11ac
Nick Peterson <peterson.nick2@gmail.com>
2016-01-27 16:22:30 -0700

    string repeat function

--- CodeCardioQuestions.js
+++ CodeCardioQuestions.js
@@ -252,6 +252,8 @@ titleCase("I'm a little tea pot");
 
 19. //Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
 
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); should return [5, 27, 39, 1001]
+
 function largestOfFour(arr) {
   var results = [];
   for (var i = 0; i < arr.length; i++) {
@@ -274,10 +276,17 @@ function largestOfFour(arr) {
   return arr.map(Function.apply.bind(Math.max, null));
 }
 
-// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); should return [5, 27, 39, 1001]
+20. //Repeat a given string (first argument) n times (second argument). Return an empty string if n is a negative number. 
+// repeat("abc", 3); should return "abcabcabc"
 
-
-
-
+function repeat(str, num) {
+  
+  var word = [];
+  
+  for (i = 0; i < num; i++) {
+    word[i] = str;
+  }
+  return word.join('');
+}
 
 
